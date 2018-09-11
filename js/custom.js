4d31003ef493864cfbe75ed36c76d5c741e0b3b2
andrew-pyle <axpyle@ualr.edu>
2018-09-11 13:33:53 -0500

    Implement Javascript tab area with display: none; & jQuery
    - Needs styling work
    - Google maps iframe jacking up width responsiveness

--- js/custom.js
+++ js/custom.js
@@ -4,47 +4,58 @@
 (function($) {
   "use strict"; // Start of use strict
 
-  // Custom Scroll Reveal for weekly schedule
-  // Use global variable `sr` from creative.js if availabl
-  if (window.sr === undefined) {
-    window.sr = ScrollReveal();
-  } else {
-    sr.reveal('div.schedule-card', {
-      duration: 600,
-      scale: 0.3,
-      distance: '0px'
-    }, 200);
-    // Above integer passed to ScrollSync.reveal() seems to be undocumented
-    // behavior. Sets an interval between the animation of each object with
-    // the specified CSS selector.
-
-  }
+  // // Custom Scroll Reveal for weekly schedule
+  // // Use global variable `sr` from creative.js if availabl
+  // if (window.sr === undefined) {
+  //   window.sr = ScrollReveal();
+  // } else {
+  //   sr.reveal('div.schedule-card', {
+  //     duration: 600,
+  //     scale: 0.3,
+  //     distance: '0px'
+  //   }, 200);
+  //   // Above integer passed to ScrollSync.reveal() seems to be undocumented
+  //   // behavior. Sets an interval between the animation of each object with
+  //   // the specified CSS selector.
+  //
+  // }
 
   // Tab Content
+  var tabContentIdPrefix = 'tab-content-';
+  var tabsJquery = $('.tab');
+  var tabContentJquery = $('.tab-content');
+  var tabContentParent = $('#tab-row');
 
+  // Select Tab Section on click
+  tabsJquery.click(function (clickedTab) {
+    var tabContentIdToShow = tabContentIdPrefix + this.id;
+    // Toggle Shown Section
+    toggleTabContentJquery(tabContentIdToShow, tabContentJquery, tabContentParent);
+    // Toggle Tab Active Status
+    var activeTabId = this.id;
+    makeActiveTab(activeTabId, tabsJquery);
+  })
 
-  var tabElements = Array.from(document.getElementsByClassName('tab'));
-  var tabContentElements = Array.from(
-    document.getElementsByClassName('tab-content')
-  );
-
-  function toggleTabContent(event, idToShow, tabElements, tabContentElements) {
+  // Toggle Shown Tab-Area Content
+  function toggleTabContentJquery(idToShow, tabContentElements, tabContentParent) {
     // Hide all elements except the one which has the id passed as argument
-    tabContentElements.forEach(function(section) {
-      if (section.id !== idToShow) {
-        section.style.display = 'none';
-      } else {
-        section.style.display = 'block';
-      }
-    })
-
-    // Remove "active" class from all tabs
-    tabElements.forEach(function(tab) {
-      tab.classList.remove('active')
-    })
-
-    // Add "active" class to clicked tab element
-    event.currentTarget.classList.add('active')
+    tabContentElements.css('display', 'none');
+    var selector = '#' + idToShow;
+    tabContentParent.find(selector).css('display', 'block')
   }
+
+  // Only one tab can be "active" at a time
+  function makeActiveTab(tabIdToActivate, allTabContentElements) {
+    // Remove "active" class from all tabs
+    allTabContentElements.removeClass('active-tab');
+    $('.tab-arrow').css('visibility', 'hidden');
+    // Add "active" class to clicked tab element
+    $('#' + tabIdToActivate).addClass('active-tab');
+    $('#' + 'tab-arrow-' + tabIdToActivate).css('visibility', 'visible')
+  }
+
+  // Set Initial Tab Area state
+  var defaultDisplayTabId = '#tutoring';
+  $(defaultDisplayTabId).trigger('click')
 
 })(jQuery); // End of use strict
